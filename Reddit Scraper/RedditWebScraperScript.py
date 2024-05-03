cda3eca54d590096db9ed952be0a1a87ad60b05a
Paimon Goulart <paimongoulart@gmail.com>
2024-05-02 23:25:15 -0700

    Loop through top hot new

--- Reddit Scraper/RedditWebScraperScript.py
+++ Reddit Scraper/RedditWebScraperScript.py
@@ -28,80 +28,89 @@ filesize = 0
 embeddedLinks = r'https?:\/\/(?:www\.)?[^\s]+'
 
 
+def scrapePostLoop(submision):
+    if(submission.id not in seenPosts):
+        #print(submission.selftext)
+        #print(submission.title)
+        #print(submission.id) 
+        #print(submission.score) 
+        #print(submission.url)
+        #print(submission.permalink)
+
+        otherUrlList = []
+        otherUrls = re.findall(embeddedLinks, submission.selftext)
+        otherUrls.append(submission.url)
+        print(otherUrls)
+
+        for i in otherUrls:
+            title = "Not Found"
+            body = "Not Found"
+            url = i
+            can_fetch = False
+        
+            try:
+                url = i.strip()
+                parsed_url = urlparse(url)
+                base_url = f"{parsed_url.scheme}://{parsed_url.netloc}"
+                rp = urllib.robotparser.RobotFileParser()
+                rp.set_url(f"{base_url}/robots.txt")
+                rp.read()
+                can_fetch = rp.can_fetch('*', url)
+        
+                if can_fetch:
+                    page = requests.get(url)  
+                    page.raise_for_status()  
+                    soup = BeautifulSoup(page.content, "html.parser")
+                    if soup.title:
+                        title = soup.title.string.strip()
+                    if soup.get_text():
+                        body = soup.get_text(strip=True)
+                    print("Title:", title, "Body:", body)
+        
+            except Exception as e:
+                logging.error(f"Error processing {url}: {e}")
+
+            otherUrlDict = {
+                "title": title,
+                "body": body,
+                "url": url
+            }
+            otherUrlList.append(otherUrlDict)
+            
+        JSONdict = {
+            "title": submission.title,
+            "self_text": submission.selftext,
+            "id": submission.id,
+            "created_utc": submission.created_utc,
+            "score": submission.score,
+            "num_comments": submission.num_comments,
+            "upvote_ratio": submission.upvote_ratio,
+            "url": submission.url,
+            "perma_link": submission.permalink,
+            "Embedded_Links": otherUrlList
+        }
+
+        seenPosts.append(submission.id)
+        output_file = open("myredditdata.json", "a")
+        postString = json.dumps(JSONdict)
+        output_file.write(postString + '\n')
+        filesize = output_file.tell()
+        
+
 while(currentSubReddit < len(subredditsToCrawl) or filesize <= 500000000):
     
     print(subredditsToCrawl[currentSubReddit], len(seenPosts))
     
-    for submission in reddit.subreddit(subredditsToCrawl[currentSubReddit]).top(limit=None):
-        
-        if(submission.id not in seenPosts):
-            #print(submission.selftext)
-            #print(submission.title)
-            #print(submission.id) 
-            #print(submission.score) 
-            #print(submission.url)
-            #print(submission.permalink)
-
-            otherUrlList = []
-            otherUrls = re.findall(embeddedLinks, submission.selftext)
-            otherUrls.append(submission.url)
-            print(otherUrls)
-
-            for i in otherUrls:
-                title = "Not Found"
-                body = "Not Found"
-                url = i
-                can_fetch = False
-            
-                try:
-                    url = i.strip()
-                    parsed_url = urlparse(url)
-                    base_url = f"{parsed_url.scheme}://{parsed_url.netloc}"
-                    rp = urllib.robotparser.RobotFileParser()
-                    rp.set_url(f"{base_url}/robots.txt")
-                    rp.read()
-                    can_fetch = rp.can_fetch('*', url)
-            
-                    if can_fetch:
-                        page = requests.get(url)  
-                        page.raise_for_status()  
-                        soup = BeautifulSoup(page.content, "html.parser")
-                        if soup.title:
-                            title = soup.title.string.strip()
-                        if soup.get_text():
-                            body = soup.get_text(strip=True)
-                        print("Title:", title, "Body:", body)
-            
-                except Exception as e:
-                    logging.error(f"Error processing {url}: {e}")
-
-                otherUrlDict = {
-                    "title": title,
-                    "body": body,
-                    "url": url
-                }
-                otherUrlList.append(otherUrlDict)
-                
-            JSONdict = {
-                "title": submission.title,
-                "self_text": submission.selftext,
-                "id": submission.id,
-                "created_utc": submission.created_utc,
-                "score": submission.score,
-                "num_comments": submission.num_comments,
-                "upvote_ratio": submission.upvote_ratio,
-                "url": submission.url,
-                "perma_link": submission.permalink,
-                "Embedded_Links": otherUrlList
-            }
-    
-            seenPosts.append(submission.id)
-            output_file = open("myredditdata.json", "a")
-            postString = json.dumps(JSONdict)
-            output_file.write(postString + '\n')
-            filesize = output_file.tell()
-            
+    for submission in reddit.subreddit(subredditsToCrawl[currentSubReddit]).new(limit=None):
+        scrapePostLoop(submission)
         if(filesize >= 500000000):
             break
-            
+    for submission in reddit.subreddit(subredditsToCrawl[currentSubReddit]).top(limit=None):
+        scrapePostLoop(submission)
+        if(filesize >= 500000000):
+            break
+    for submission in reddit.subreddit(subredditsToCrawl[currentSubReddit]).hot(limit=None):
+        scrapePostLoop(submission)
+        if(filesize >= 500000000):
+            break
     currentSubReddit+=1
